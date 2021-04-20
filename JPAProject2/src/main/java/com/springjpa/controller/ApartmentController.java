6351e9c138d906de7a0545060be70565e71d64ae
abhigyan1 <abhigyandzapper@github.com>
2021-04-20 16:50:38 +0530

    adding rest endpoint /apt/{memberid}

--- JPAProject2/src/main/java/com/springjpa/controller/ApartmentController.java
+++ JPAProject2/src/main/java/com/springjpa/controller/ApartmentController.java
@@ -1,10 +1,12 @@
 package com.springjpa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +31,9 @@ HouseMemberJPA houseMemberJPA;
 	public ResponseEntity<List<HouseMembers>> getapartmentRecord() {
 		return ResponseEntity.ok(houseMemberJPA.findAll());
 	}
+	@GetMapping("/apt/{memberid}")
+	public ResponseEntity<Optional<HouseMembers>> getapartmentRecord(@PathVariable long memberid) {
+		return ResponseEntity.ok(houseMemberJPA.findById(memberid));
+	}
+	
 }
